42cc9f280111592538c916117e5173fe0148b80f
LNZ <35008288+WalkingGarbage@users.noreply.github.com>
2021-05-04 17:02:23 +0200

    Fixed bsp-layout

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -30,6 +30,18 @@ run sxhkd -c ~/.config/bspwm/sxhkd/sxhkdrc &
 #numlockx on &
 #blueberry-tray &
 picom --config $HOME/.config/bspwm/picom.conf &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
-/usr/lib/xfce4/notifyd/xfce4-notifyd &
+#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+#/usr/lib/xfce4/notifyd/xfce4-notifyd &
 #nitrogen --restore &
+
+#Monitor config
+bsp-layout set tall 1 &
+bsp-layout set tall 2 &
+bsp-layout set tall 3 &
+bsp-layout set tall 4 &
+bsp-layout set tall 5 &
+bsp-layout set tall 6 &
+bsp-layout set tall 7 &
+bsp-layout set tall 8 &
+bsp-layout set tall 9 &
+bsp-layout set tall 10 &
