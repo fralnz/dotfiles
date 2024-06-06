dfe28dd7ac36f5a695beeb443e8f0e6e39dc7977
fralnz <francesco@fragiolanza.eu>
2024-06-06 22:23:08 +0200

    moved folders

---
+++ .config/zsh/tabcomplete.zsh
@@ -0,0 +1,7 @@
+# Basic auto/tab complete:
+autoload -Uz compinit
+zstyle ':completion:*' menu select
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+zmodload zsh/complist
+compinit
+_comp_options+=(globdots)		# Include hidden files.
