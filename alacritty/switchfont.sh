07d0845a276db25aa606e9daf3e7d70f4bd6e129
fralnz <francesco@fragiolanza.eu>
2024-06-06 22:23:56 +0200

    moved to fodler

--- alacritty/switchfont.sh
+++
@@ -1,4 +0,0 @@
-# !/bin/bash
-mv ~/.config/alacritty/font.yml ~/.config/alacritty/font2.yml
-mv ~/.config/alacritty/font1.yml ~/.config/alacritty/font.yml
-mv ~/.config/alacritty/font2.yml ~/.config/alacritty/font1.yml
